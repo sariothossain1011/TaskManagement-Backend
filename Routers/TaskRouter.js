356c9aa31ff622f1959afccf883b3d5e916f708c
AppAMP-001 <shariothossian10112003@gmail.com>
2023-01-20 22:03:21 +0600

    update

--- Routers/TaskRouter.js
+++ Routers/TaskRouter.js
@@ -5,7 +5,7 @@ const Authentication = require('../Middleware/Authentication')
 
 router.post('/createTask',Authentication,TaskControll.createTask)
 router.delete('/deleteTask/:id',Authentication,TaskControll.deleteTask)
-router.put('/updateStatusTask/:id/:status',Authentication,TaskControll.updateStatusTask)
+router.get('/updateStatusTask/:id/:status',Authentication,TaskControll.updateStatusTask)
 router.get('/listTaskByStatus/:status',Authentication,TaskControll.listTaskByStatus)
 router.get('/taskStatusCount',Authentication,TaskControll.taskStatusCount)
 
