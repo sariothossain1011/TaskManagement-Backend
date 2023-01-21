dfb2e6fddbc28e30b436ef3bb092310da86e5816
AppAMP-001 <shariothossian10112003@gmail.com>
2023-01-21 14:02:29 +0600

    controller edit

--- Controllers/UserControll.js
+++ Controllers/UserControll.js
@@ -60,16 +60,31 @@ const profileUpdate =async(req,res)=>{
     }
 }
 
-const profileDetails =async(req,res)=>{
-    let email=req.headers['email'];
+// const profileDetails =async(req,res)=>{
+//     let email=req.headers['email'];
+//     await UserModel.aggregate([
+//         {$match:{email:email}},
+//         {$project:{_id:1,email:1,firstName:1,lastName:1,mobile:1,photo:1,password:1}}
+//     ],(error,data)=>{
+//         if(error){
+//             res.status(400).json({status:"fail",data:error})
+//         }else{
+//                 res.status(200).json({status:"success",data:data})
+//         }
+//     })
+// }
+
+const profileDetails =(req,res)=>{
+    let email= req.headers['email'];
     UserModel.aggregate([
         {$match:{email:email}},
-        {$project:{_id:1,firstName:1,lastName:1,mobile:1,photo:1,password:1}}
-    ],(error,data)=>{
-        if(error){
-            res.status(400).json({status:"fail",data:error})
-        }else{
-                res.status(200).json({status:"success",data:data})
+        {$project:{_id:1,email:1,firstName:1,lastName:1,mobile:1,photo:1,password:1}}
+    ],(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
         }
     })
 }
